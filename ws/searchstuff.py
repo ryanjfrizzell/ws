15e973a990f0a94d178ec0828f8057fcd147cc7e
Ryan Frizzell <ryan.frizzell@homeadvisor.com>
2019-04-03 12:58:02 -0600

    remove fail stuff

--- ws/searchstuff.py
+++ ws/searchstuff.py
@@ -6,26 +6,10 @@ class SearchStuff:
         self.mrix = list_matrix
         try:
             self.wl = io.open(word_list, 'r')
+            self.searchParams = self.wl.readlines()
         except Exception as e:
             raise("failed to open file %s" % e)
 
-    def search_for_str(self, search_string, line):
-        """
-        to make this simple we'll assume we're searching the line
-        ffzzcatzz
-        :param search_string: the string to look for ex: cat in ffzzcatzz
-        :param line: the line we're looking in ex: ffzzcatzz
-        :return: boolean if search_string exists in line
-
-        did some quick comparisons running time on regex vs
-        string in line. Tests were done extremely simply
-        (bash) time testscript.py
-        testscript.py contained very simple examples
-        """
-        ln = line.lower().strip()
-        st = search_string.lower().strip()
-        if ln in st:
-            return True
 
     def flatten_lst(self, lst):
         return ''.join(lst)
@@ -43,12 +27,27 @@ class SearchStuff:
             self.s.append(self.flatten_lst(z))
 
         for m in self.s:
-            self.found = self.find_str_in_list(m, self.wl)
+            self.find_str_in_list(m, self.searchParams)
 
-        print(self.found)
 
     def find_str_in_list(self, srch, sl):
-        for z in sl.readlines():
-            print(z)
-            if self.search_for_str(srch, z):
-                return srch
+        """
+        to make this simple we'll assume we're searching the line
+        ffzzcatzz
+        :param search_string: the string to look for ex: cat in ffzzcatzz
+        :param line: the line we're looking in ex: ffzzcatzz
+        :return: boolean if search_string exists in line
+
+        did some quick comparisons running time on regex vs
+        string in line. Tests were done extremely simply
+        (bash) time testscript.py
+        testscript.py contained very simple examples
+        """
+
+        for z in sl:
+            ln = srch.lower().strip()
+            st = z.lower().strip()
+            if st in ln:
+                print("found %s in %s" % (st, ln))
+            else:
+                pass
